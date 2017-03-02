0be8b98745ff6acc3f3459c2cebe4277875155ea
david martin del campo vergara <david martin del campo vergara>
2017-03-02 17:20:15 -0600

    Se inicio el proyecto con javascript

---
+++ js/calculadora.js
@@ -0,0 +1,13 @@
+var num1, num2, res;
+num1 = document.getElementById("numero_1");
+num2 = document.getElementById("numero_2");
+
+function suma(){
+	res = num1.value + num2.value;
+}
+function resta(){
+	res = num1.value - num2.value;
+}
+resultado.addEventListener("click", function(){
+	if () {}
+});
---
+++ js/index.js
@@ -0,0 +1,12 @@
+boton_1.addEventListener("click", function(){
+document.getElementById("link1").scrollIntoView();
+});
+boton_2.addEventListener("click", function(){
+document.getElementById("link2").scrollIntoView();
+});
+boton_3.addEventListener("click", function(){
+document.getElementById("link3").scrollIntoView();
+});
+boton_4.addEventListener("click", function(){
+document.getElementById("link4").scrollIntoView();
+});
