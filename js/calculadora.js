01405ebe7ba721ee5df4088b16b4b8b480c162a5
david martin del campo vergara <david martin del campo vergara>
2017-03-07 17:17:53 -0600

    se agrego el js de calculadora, el sistema ya casi funciona pero tiene un error en la advertencia de que no hay datos, hay que arreglarlo

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,6 +1,41 @@
-var num1, num2, res, f;
-if (getElementById("suma") || document.getElementById("resta")) {
-	num1 = document.getElementById("numero_1");
-	num2 = document.getElementById("numero_2");
-	window.alert(num1);
+var n1, n2, res, i;
+
+function operar(tipo) {
+	n1 = document.getElementById("numero_1").value;
+	n2 = document.getElementById("numero_2").value;
+
+	if (n1 != "" && n2 !="") {
+		if (tipo == 1) {
+			document.getElementById("resultado").innerHTML = parseInt(num1) + parseInt(num2);
+		}
+		else
+			if (tipo == 2) {
+				document.getElementById("resultado").innerHTML = parseInt(num1) - parseInt(num2);
+			}
+	}
+	else{
+		if (n1 == "") {
+			alerta(1);
+		}
+		else {
+			document.getElementById("er1").style.display = "none";
+		}
+		if (n2 == "") {
+			alerta(2);
+		}
+		else {
+			document.getElementById("er1").style.display = "none";
+		}
+	}
+}
+
+function alerta(i) {
+	switch(i){
+		case 1: 
+			document.getElementById("er1").style.display = "initial";
+			break;
+		case 2: 
+			document.getElementById("er2").style.display = "initial";
+	}
+	document.getElementById("resultado").innerHTML = "";
 }
