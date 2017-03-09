8dc85d2e6f5a67f0e1da3c28206e90d206158c90
david martin del campo vergara <david martin del campo vergara>
2017-03-09 17:16:28 -0600

    Se termino la calculadora. Se agregó un archivo html y un archivo js llamados piramide. Este es otro ejemplo en el que usuario ofrece un numeron y aparece una piramide de ceros de dimension n

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,41 +1,36 @@
-var n1, n2, res, i;
+svar n1, n2, res, i;
 
-function operar(tipo) {
+function operar(tipo)
+{
 	n1 = document.getElementById("numero_1").value;
 	n2 = document.getElementById("numero_2").value;
 
-	if (n1 != "" && n2 !="") {
-		if (tipo == 1) {
-			document.getElementById("resultado").innerHTML = parseInt(num1) + parseInt(num2);
+	if (n1 != "" && n2 !="")
+	{
+		document.getElementById("er1").style.display = "none";
+		document.getElementById("er2").style.display = "none";
+		if (tipo ==1)
+		{
+			document.getElementById("resultado").innerHTML = parseInt(n1) + parseInt(n2);
 		}
 		else
-			if (tipo == 2) {
-				document.getElementById("resultado").innerHTML = parseInt(num1) - parseInt(num2);
-			}
-	}
-	else{
-		if (n1 == "") {
-			alerta(1);
-		}
-		else {
-			document.getElementById("er1").style.display = "none";
-		}
-		if (n2 == "") {
-			alerta(2);
-		}
-		else {
-			document.getElementById("er1").style.display = "none";
+		{
+			document.getElementById("resultado").innerHTML = parseInt(n1) - parseInt(n2);
 		}
 	}
-}
-
-function alerta(i) {
-	switch(i){
-		case 1: 
+	else
+	{
+		if (n1 == "")
+		{
 			document.getElementById("er1").style.display = "initial";
-			break;
-		case 2: 
+		}
+		else
+			document.getElementById("er1").style.display = "none";
+		if (n2 == "")
+		{
 			document.getElementById("er2").style.display = "initial";
+		}
+		else
+			document.getElementById("er2").style.display = "none";
 	}
-	document.getElementById("resultado").innerHTML = "";
 }
---
+++ js/piramide.js
@@ -0,0 +1,29 @@
+var n, i, j, res;
+
+function comprobar() 
+	{
+	n = parseInt(document.getElementById("num").value);
+	if (n < 1) 
+	{
+		alert("numero no válido");
+	}
+	else
+	{
+		res = "";
+		piramide();
+	}
+}
+
+function piramide() 
+{
+	for(i = 1; i <= n; i++)
+	{
+		res = res + "<p>"
+		for (j = 0; j < i; j++)
+		{
+			res = res + 0;
+		}
+		res = res + "</p>"
+	}
+	document.getElementById("piramide").innerHTML = res;
+}
