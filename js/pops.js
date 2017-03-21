3651927cba41823b9a6b52fc5b9689f7abadadbc
david martin del campo vergara <david martin del campo vergara>
2017-03-21 16:48:48 -0600

    version para probar los media queries en un celular

--- js/pops.js
+++ js/pops.js
@@ -6,11 +6,10 @@ function aparece() {
 	num = Math.floor(Math.random() * 1000000);
 	res = res + '<div class="externo" id="' + 
 				num +
-				'"><br>' +
-				'<div><img src="../img/platon.jpg"></div><br>' + 
+				'">' +
+				'<div><img src="../img/platon.jpg"></div>' + 
 				'<div class="interno"><p>#' + 
 				num +
-				'</p></div></div><br>';
+				'</p></div></div>';
 	document.getElementById("cuadros").innerHTML = res;
-	document.getElementById(String(num)).style.cssFloat = "right";
 }
