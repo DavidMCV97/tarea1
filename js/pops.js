6509e49f56e2af6aef716bc25fde5f663cb573ea
david martin del campo vergara <david martin del campo vergara>
2017-03-28 17:32:21 -0600

    como continuación del proyecto, se agregó un boton a las imagenes de pop

--- js/pops.js
+++ js/pops.js
@@ -4,12 +4,14 @@ res = "";
 
 function aparece() {
 	num = Math.floor(Math.random() * 1000000);
-	res = res + '<div class="externo" id="' + 
-				num +
-				'">' +
+	res = res + '<div class="externo" id="' + num +'">' +
+				'<button class="tache" onclick="desaparece()">x</button>' +
 				'<div><img src="../img/platon.jpg"></div>' + 
-				'<div class="interno"><p>#' + 
-				num +
-				'</p></div></div>';
+				'<div class="interno"><p>#' + num + '</p></div>' +
+				'</div>';
 	document.getElementById("cuadros").innerHTML = res;
+}
+
+function desaparece() {
+	document.removeChild(getElementById(""));
 }
