8d820de2af24c2f13036df933bf54716d396029d
david martin del campo vergara <david martin del campo vergara>
2017-04-25 17:54:44 -0500

    Casi se termina el proyecto idiomas. los cuadros se terminaron, solo falta imprimir con el boton aceptar

--- js/idiomas.js
+++ js/idiomas.js
@@ -1,3 +1,9 @@
+var code, i, j, pub, idio, selector, elementos, res, tit;
+selector = document.getElementById("selIdio");
+pub = document.getElementById("publish");
+res = "";
+elementos = new Array("0");
+
 function overAcept() {
 	document.getElementById("btnAceptar").style.color = "yellow";
 	document.getElementById("btnAceptar").style.backgroundColor = "black";
@@ -9,16 +15,33 @@ function outAcept() {
 function desaparece(j) {
 	elem = document.getElementById(j);
 	elem.parentNode.removeChild(elem);
+	selector.options[j].style.display = "list-item";
 }
-
-var code, id; 
 function display() {
-	id = Math.floor(Math.random() * 1000000);
-	code = 	'<div class="divExterno" id="aleatorio">'
-			+ '<div class="divTitulo">Idioma</div>'
-			+ '<button class="btnCruz" onclick="desaparece(&#39;aleatorio&#39;)">x</button>'
-			+ '<div class="divInterno">'
-			+ 'Nivel:<select>'
-			+ '<option>Avanzado</option><option>Intermedio</option><option>Principiante</option>'
-			+ '</select>'
-			+ '</div></div>';
+	i = selector.selectedIndex;
+	idio = selector.options[selector.selectedIndex].text;
+	
+	code = document.createElement("div");
+	code.class = "divExterno";
+	code.id = i;
+	code.innerHTML = 	  '<div class="divExterno" id = "' + i + '">'
+						+ '<div class="divTitulo" id = "tit'+ i +'">' + idio + '</div>'
+						+ '<button class="btnCruz" onclick="desaparece(&#39;' + i + '&#39;)">x</button>'
+						+ '<div class="divInterno">'
+						+ 'Nivel:<select>'
+						+ '<option>Avanzado</option><option>Intermedio</option><option>Principiante</option>'
+						+ '</select>'
+						+ '</div></div>';
+	pub.appendChild(code);
+
+	elementos.push(i);
+	selector.selectedIndex = 0;
+	selector.options[i].style.display = "none";
+}
+function escribe() {
+	for (j = 1; j < elementos.length; j++){
+		tit = "tit" + elementos[j];
+		res = res + document.getElementById(tit).textContent;
+	}
+	document.getElementById("texto").innerHTML = res;
+}
