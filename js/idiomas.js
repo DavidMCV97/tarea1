3d77b35fd4c4f8517a08e4cce6fa59dfadd3042b
david martin del campo vergara <david martin del campo vergara>
2017-05-02 17:27:27 -0500

    El vector ya funciona, solo falta imporimir los niveles

--- js/idiomas.js
+++ js/idiomas.js
@@ -1,8 +1,8 @@
-var code, i, j, pub, idio, selector, elementos, res, tit;
+var code, i, j, k, pub, idio, selector, elementos, res, tit, aux;
 selector = document.getElementById("selIdio");
 pub = document.getElementById("publish");
 res = "";
-elementos = new Array("0");
+elementos = new Array("0");	//guarda los números correspondientes a cada idioma desplegado
 
 function overAcept() {
 	document.getElementById("btnAceptar").style.color = "yellow";
@@ -16,6 +16,7 @@ function desaparece(j) {
 	elem = document.getElementById(j);
 	elem.parentNode.removeChild(elem);
 	selector.options[j].style.display = "list-item";
+	remueve(j);
 }
 function display() {
 	i = selector.selectedIndex;
@@ -25,13 +26,13 @@ function display() {
 	code.class = "divExterno";
 	code.id = i;
 	code.innerHTML = 	  '<div class="divExterno" id = "' + i + '">'
-						+ '<div class="divTitulo" id = "tit'+ i +'">' + idio + '</div>'
-						+ '<button class="btnCruz" onclick="desaparece(&#39;' + i + '&#39;)">x</button>'
-						+ '<div class="divInterno">'
-						+ 'Nivel:<select>'
-						+ '<option>Avanzado</option><option>Intermedio</option><option>Principiante</option>'
-						+ '</select>'
-						+ '</div></div>';
+	+ '<div class="divTitulo" id = "tit'+ i +'">' + idio + '</div>'
+	+ '<button class="btnCruz" onclick="desaparece(&#39;' + i + '&#39;)">x</button>'
+	+ '<div class="divInterno">'
+	+ 'Nivel:<select>'
+	+ '<option>Avanzado</option><option>Intermedio</option><option>Principiante</option>'
+	+ '</select>'
+	+ '</div></div>';
 	pub.appendChild(code);
 
 	elementos.push(i);
@@ -39,9 +40,29 @@ function display() {
 	selector.options[i].style.display = "none";
 }
 function escribe() {
-	for (j = 1; j < elementos.length; j++){
-		tit = "tit" + elementos[j];
-		res = res + document.getElementById(tit).textContent;
+	document.getElementById("texto").innerHTML = "";
+	res = "";
+	if (elementos.length == 1) {res = "seleccione un idioma, por favor.";}
+	else{
+		for (j = 1; j < elementos.length; j++){
+			tit = "tit" + elementos[j];
+			res = res + "idioma: " + document.getElementById(tit).textContent + ";&nbsp nivel: " + "<br>";
+		}
 	}
 	document.getElementById("texto").innerHTML = res;
+}
+function remueve(j) {
+	k = 0;
+	while (elementos[k] != j){
+		k++;
+		if (k>elementos.length) {alert("Error raro, checa aquí!!"); break;}
+	}
+	if(k != elementos.length - 1){
+		aux = elementos[elementos.length - 1];
+		elementos[k] = aux;
+		elementos.pop();
+	}
+	else{
+		elementos.pop();
+	}
 }
